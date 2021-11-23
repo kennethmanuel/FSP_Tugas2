32eb26098155c4c2a5de4d846008dd492a75319a
jehudaRivaldoS <71364423+jehudaRivaldoS@users.noreply.github.com>
2021-11-24 00:16:21 +0700

    coba2

---
+++ Screen/gamearea.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tic Tac Toe</title>
+	<style type="text/css">
+		td 
+		{
+			border: 1px solid black;
+			height: 50px;
+			width: 50px;
+		}
+	</style>
+</head>
+<body>
+	<table>
+    	<tr>
+        	<td></td>
+        	<td></td>
+        	<td></td>
+    	</tr>
+    	<tr>
+        	<td></td>
+        	<td></td>
+        	<td></td>
+    	</tr>
+    	<tr>
+        	<td></td>
+        	<td></td>
+        	<td></td>
+    	</tr>
+	</table>
+</body>
+</html>
